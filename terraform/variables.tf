b4148995dbb3d75fe4742f2a881f644cbc6efbd1
U210709005 <emre-parlak2002@hotmail.com>
2026-08-05 22:19:57 +0300

    feat(github): add codeowners, editorconfig, and bilingual guidelines

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,5 +7,5 @@ variable "github_token" {
 variable "github_org_name" {
   type        = string
   description = "GitHub Organization Name"
-  default     = "your-org" 
+  default     = "your-org"
 }
